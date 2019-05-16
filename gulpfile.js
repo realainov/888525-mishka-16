d59007b9fac2a301a6a627a50981fdf3df7c476a
Юрий <realainov@ya.ru>
2019-05-17 00:04:11 +0300

    Изменяет автоматизацию проекта и добавляет svg спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,37 @@
 "use strict";
 
 const gulp = require("gulp");
+const plumber = require("gulp-plumber");
 const sourcemaps = require("gulp-sourcemaps");
+const browsersync = require("browser-sync").create();
+
+const del = require("del");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+
 const pug = require("gulp-pug");
-const beautify = require('gulp-beautify');
-const htmlmin = require("gulp-html-minifier");
+
 const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
+
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sorting = require("postcss-sorting");
-const cssnano = require("gulp-cssnano");
-const uglify = require("gulp-uglify");
-const concat = require("gulp-concat");
+
+const uglify = require("gulp-uglify-es").default;
+
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
-const svgSprite = require('gulp-svg-sprite');
-const rename = require("gulp-rename");
-const browsersync = require("browser-sync").create();
+const svgstore = require('gulp-svgstore');
 
-gulp.task("pug", function () {
+gulp.task("pages", function () {
   return gulp.src("source/pug/*.pug")
-    .pipe(pug({pretty: false}))
-    .pipe(beautify.html({
-      "indent_size": 2,
-      "indent_with_tabs": false,
-      "end-with-newline": true,
-      "inline": [],
-      "unformatted": [],
-      "content_unformatted": [],
-      "extra_liners": [],
-      "editorconfig": true
-    }))
-    .pipe(gulp.dest("source"));
-});
-
-gulp.task("html-build", function () {
-  return gulp.src("source/*.html")
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(plumber())
+    .pipe(pug({pretty: true}))
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("sass", function () {
+gulp.task("styles", function () {
 
   const processors = [
     autoprefixer({browsers: ["last 2 version"]}),
@@ -59,69 +50,89 @@ gulp.task("sass", function () {
   ];
 
   return gulp.src(["source/sass/style.scss"])
+    .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(sass().on("error", sass.logError))
+    .pipe(sass.sync().on("error", sass.logError))
     .pipe(postcss(processors))
-    .pipe(beautify.css({
-      "indent_size": 2,
-      "indent_with_tabs": false,
-      "end-with-newline": true
-    }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("source/css"));
-});
-
-gulp.task("css-build", function () {
-  return gulp.src("source/css/style.css")
     .pipe(gulp.dest("build/css"))
     .pipe(cssnano())
+    .pipe(sourcemaps.write("."))
     .pipe(rename({suffix: ".min"}))
     .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("js", function () {
+gulp.task("scripts", function () {
   return gulp.src(["source/js/**/*.js", "!source/js/app.js"])
+    .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(concat("app.js"))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("source/js"));
-});
-
-gulp.task("js-build", function () {
-  return gulp.src("source/js/app.js")
-    .pipe(gulp.dest("build/js"))
+    .pipe(concat("app.min.js"))
     .pipe(uglify())
-    .pipe(rename({suffix: ".min"}))
+    .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("build/js"));
 });
 
-gulp.task("img", function () {
-  return gulp.src("source/img/*")
-    .pipe(imagemin())
-    .pipe(gulp.dest("source/img"))
+gulp.task("images", function () {
+  return gulp.src("source/img/*.{jpg,jpeg,png,gif.svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo(),
+      imagemin.gifsicle()
+    ]))
+    .pipe(gulp.dest("build/img"))
     .pipe(webp({lossless: true}))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/*.svg")
+    .pipe(svgstore({inlineSvg: true}))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/*.{png,jpg,jpeg,gif,svg}"
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("browser-sync", function () {
   browsersync.init({
     server: {
-      baseDir: "source"
+      baseDir: "build"
     }
   });
-  browsersync.watch(["source/*.html", "source/css/style.css", "source/js/app.js", "source/img/**/*"], browsersync.reload);
+  browsersync.watch(["build/*.html", "build/css/style.min.css", "build/js/app.min.js", "build/img/**/*", "build/fonts/**/*"], browsersync.reload);
 });
 
-gulp.task("source", function () {
-  gulp.watch("source/pug/**/*.pug", gulp.parallel("pug"));
-  gulp.watch("source/img/*", gulp.parallel("pug", "img"));
-  gulp.watch("source/sass/**/*.scss", gulp.parallel("sass"));
-  gulp.watch(["source/js/**/*.js", "!source/js/app.js"], gulp.parallel("js"));
-});
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "pages",
+  "images",
+  "sprite",
+  "styles",
+  "scripts"
+));
 
-//gulp.task("build", gulp.parallel("html-build", "css-build", "js-build", "img-build"));
+gulp.task("server", function () {
+  gulp.watch("source/pug/**/*.pug", gulp.parallel("pages"));
+  gulp.watch("source/img/*", gulp.series("images", "sprite"));
+  gulp.watch("source/sass/style.scss", gulp.parallel("styles"));
+  gulp.watch(["source/js/**/*.js", "!source/js/app.js"], gulp.parallel("scripts"));
+});
 
 gulp.task("default", gulp.series(
-  gulp.parallel("pug", "sass", "js", "img"),
-  gulp.parallel("source", "browser-sync")
+  gulp.series("clean", "copy", "images", "sprite", "pages", "styles", "scripts"),
+  gulp.parallel("server", "browser-sync")
 ));
--- source/js/app.js
+++ source/js/app.js
@@ -1,30 +1,24 @@
 if (document.querySelector(".contacts__map") !== null) {
   ymaps.ready(function () {
-    let myMap = new ymaps.Map('map', {
+    let myMap = new ymaps.Map("map", {
         center: [59.938886, 30.323159],
         zoom: 16
       }, {
-        searchControlProvider: 'yandex#search'
+        searchControlProvider: "yandex#search"
       }),
 
-      // Создаём макет содержимого.
       MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        "<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>"
       ),
 
       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
         hintContent: 'Собственный значок метки',
         balloonContent: 'Это красивая метка'
       }, {
-        // Опции.
-        // Необходимо указать данный тип макета.
+
         iconLayout: 'default#image',
-        // Своё изображение иконки метки.
         iconImageHref: 'img/icon-map-pin.svg',
-        // Размеры метки.
         iconImageSize: [67, 100],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
         iconImageOffset: [-45, -85]
       });
 
